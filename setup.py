9e00f0c06d20ceae7e01dc2ef73366b29f4be0e7
Jaime Buelta <jaime.buelta@gmail.com>
2013-06-08 17:59:19 +0100

    Add info to push to PyPI, including explicit license

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ setup(
     # metadata
     name='ffind',
     description='Sane replacement for find',
-    license='Public domain',
+    license='MIT',
     version='0.1',
     author='Jaime Buelta',
+    author_email='jaime.buelta@gmail.com',
+    url='https://github.com/jaimebuelta/ffind',
+    download_url='https://github.com/jaimebuelta/ffind/tarball/0.1',
     platforms='Cross Platform',
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
+    keywords=['searching', 'file system'],
     packages=['ffind'],
     scripts=['scripts/ffind'],
 )

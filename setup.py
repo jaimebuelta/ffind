1c43214eb0e0a0d8d789e1ced6b6466d4d077fbf
Jaime Buelta <jaime.buelta@gmail.com>
2013-07-13 14:47:12 +0100

    Set as version 0.4

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     name='ffind',
     description='Sane replacement for command line file search',
     license='MIT',
-    version='0.3.1',
+    version='0.4',
     author='Jaime Buelta',
     author_email='jaime.buelta@gmail.com',
     url='https://github.com/jaimebuelta/ffind',
-    download_url='https://github.com/jaimebuelta/ffind/tarball/0.3',
+    download_url='https://github.com/jaimebuelta/ffind/tarball/0.4',
     platforms='Cross Platform',
     classifiers=[
         'Programming Language :: Python :: 2',
